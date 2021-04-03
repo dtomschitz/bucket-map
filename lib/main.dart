b4fd69cc61f013b27c15d303b94ea7e811ddc656
Christof Schwarzenberger <cs267@hdm-stuttgart.de>
2021-04-03 13:35:54 +0200

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:toggle_bar/toggle_bar.dart';
 
 void main() => runApp(MyApp());
@@ -38,21 +37,37 @@ class _DemoAppState extends State<DemoApp> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             ToggleBar(
-                labels: labels,
-                textColor: Colors.white,
-                backgroundColor: Colors.deepPurple,
-                selectedTabColor: Colors.purpleAccent,
-                labelTextStyle: TextStyle(fontWeight: FontWeight.bold),
-                onSelectionUpdated: (index) {
-                  setState(() {
-                    counter = index;
-                  });
-                }
+              labels: labels,
+              textColor: Colors.white,
+              backgroundColor: Colors.deepPurple,
+              selectedTabColor: Colors.purpleAccent,
+              labelTextStyle: TextStyle(fontWeight: FontWeight.bold),
+              onSelectionUpdated: (index) {
+                setState(() {
+                  counter = index;
+                });
+              }
               ),
             SizedBox(height: 200,),
             Text(labels[counter], style: TextStyle(fontWeight: FontWeight.bold,fontSize: 50, color: Colors.black),),
           ],
         ),
+      ),
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.menu_close,
+        backgroundColor: Colors.blue,
+        children: [
+          SpeedDialChild(
+            child: Icon(Icons.flag),
+            label: "add Country",
+            onTap: () => print("new Country"),
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.push_pin),
+            label: "add Pin",
+            onTap: () => print("new Pin"),
+          )
+        ],
       ),
     );
   }
