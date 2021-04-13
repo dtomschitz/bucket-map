a171cf4fda154fc9c685c2372f6be7a386fd1d34
David Tomschitz <david@tomschitz.de>
2021-04-13 16:40:09 +0200

    added the option for filtering countries

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,18 @@ import 'package:flutter/widgets.dart';
 import 'package:bucket_map/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:location_permissions/location_permissions.dart';
-import 'package:permissions_plugin/permissions_plugin.dart' as permissions_plugin;
 
 void main() async {
   Bloc.observer = SimpleBlocObserver();
 
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  
-  PermissionStatus permission =
-      await LocationPermissions().requestPermissions();
-
-  Map<permissions_plugin.Permission, permissions_plugin.PermissionState> permission_read_phone_state =
-      await permissions_plugin.PermissionsPlugin.requestPermissions(
-          [permissions_plugin.Permission.READ_PHONE_STATE]);
-
 
   runApp(
     BlocProvider(
       create: (context) {
         return CountriesBloc()..add(LoadCountriesEvent());
+
       },
       child: App(),
     ),
--- lib/widgets/countries_map.dart
+++ lib/widgets/countries_map.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:bucket_map/config/constants/constants.dart';
 import 'package:flutter/foundation.dart';
@@ -26,11 +28,22 @@ class _CountriesMapState extends State<CountriesMap> {
     super.dispose();
   }
 
-  void _onMapCreated(MapboxMapController controller) {
+  void _onMapCreated(MapboxMapController controller) async {
     _mapController = controller;
   }
 
-  void _onStyleLoadedCallback() {}
+  void _onStyleLoadedCallback() {
+    _mapController.setFilter(
+      'country-boundaries',
+      [
+        "match",
+        ["get", "iso_3166_1_alpha_3"],
+        ["NLD", "AFG", "ALA", "ALB", "ASM", "ATA", "AIA", "BHR", "BGD", "BLR", "BEL", "BWA", "BRA", "BES"],
+        true,
+        false
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +51,12 @@ class _CountriesMapState extends State<CountriesMap> {
       accessToken: AppConstants.MAPBOX_ACCESS_TOKEN,
       initialCameraPosition: const CameraPosition(target: LatLng(0.0, 0.0)),
       styleString: new File("assets/style.json").path,
+      //styleString: 'mapbox://styles/daviddo3399/cknbq7w7y0vlc17r0i37seyc3',
       onMapCreated: _onMapCreated,
+      onMapIdle: () {
+        print("dawdd");
+      
+      },
       onStyleLoadedCallback: _onStyleLoadedCallback,
     );
   }
