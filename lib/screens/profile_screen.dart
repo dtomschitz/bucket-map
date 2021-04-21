1aafc1d1b4a2ed52fa0a9a0a414a9437f5351fbf
Duane <duane.englert@gmx.de>
2021-04-21 15:32:17 +0200

    added profile_screens and changed screens.dart

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,12 @@
+import 'package:bucket_map/screens/screens.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+    );
+  }
+}
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -2,3 +2,4 @@ export 'countries.dart';
 export 'country_search.dart';
 export 'create_pin.dart';
 export 'settings_screen.dart';
+export 'profile_screen.dart';
